d21d200f0e08b2c6cdcb429b8580ce7dd553a572
salesyacine-code <salessyacine@gmail.com>
2026-04-16 11:42:21 +0100

    done

---
+++ src/main/java/org/tp3/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package org.tp3.repository;
+
+
+
+import org.tp3.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
